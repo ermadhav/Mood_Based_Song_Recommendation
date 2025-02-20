563725cf2426d398cc7005f3cb7479b92e356cf0
ermadhav <madhavtiwari2024@gmail.com>
2025-02-20 23:20:08 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Madhav Tiwari
+          
         </p>
         
       </header>
